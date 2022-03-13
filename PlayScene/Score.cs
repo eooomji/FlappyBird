7aa7d6ac1a4c11d9c8a8bda146a0b134457d9001
엄지 <70639677+eooomji@users.noreply.github.com>
2022-03-13 15:10:03 +0900

    점수를 가져오는 코드

---
+++ PlayScene/Score.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI를 가져올 때
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = score.ToString(); // C#에도 ToString이..!!!! int -> string으로 바꿔줄 때 사용
+    }
+}
